79c2de1a7ba0d9d5ffc615f0ec0a80fd23ce7d2f
Florent POUJOL <florent.poujol@gmail.com>
2014-02-16 10:57:03 +0100

    Put css and js files in the asset folder, changed back to top button

--- template.php
+++ template.php
@@ -11,7 +11,7 @@
         <meta name="description" content="A series of tutorials that aim to built a Minecraft-like with with the cooperative game making platform CraftStudio.">
         <meta name="keywords" content="CraftStudio, gamedev, game development, Minecraft, tutorial, florent, poujol">
 
-        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $indexUrl; ?>assets/css/stylesheet.css">
         <title><?php echo $pageTitle; ?> - Crafting Minecraft in CraftStudio</title>
     </head>
     <body>
@@ -31,6 +31,6 @@ else
         </footer>
 
         <script language="javascript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-        <script language="javascript" type="text/javascript" src="<?php echo $indexUrl; ?>scroll_minecraft.js"></script>
+        <script language="javascript" type="text/javascript" src="<?php echo $indexUrl; ?>assets/js/script.js"></script>
     </body>
 </html>
